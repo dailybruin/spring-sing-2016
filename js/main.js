a310725ea877b076bc38ae0f477f136b56663e7d
jbrundrett <julien.brundret@gmail.com>
2016-05-20 04:02:28 -0700

    lol got videos working

--- js/main.js
+++ js/main.js
@@ -1,34 +1,45 @@
 var onlanding = 1;
+
 // YT API
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
-Handlebars.registerHelper("formatBodyText", function(t) {
-	t = t.trim();
-	var re = new RegExp('[\r\n]+', 'g');
-    // return (t.length>0?'<p>'+t.replace(re,'</p><p>')+'</p>':null);
-    if (t.length > 0) {
-    	var ret_val = '';
-	    for (var i = 0; i < t.length; i++) { 
-	        if (t.codePointAt(i) > 127) {
-	            ret_val += '&#' + t.codePointAt(i) + ';';
-	        } else {
-	            ret_val += t.charAt(i);
-	        }
-	    }
-    	return ('<p>'+ret_val.replace(re,'</p><p>')+'</p>');
-
-    }
-    else {
-    	return (null);
-    }
-});
-
+// YT embed code
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		width: '100%',
+		videoId: 'QmlUKVz4nrk',
+	});
+}
 
 $(document).ready(function() {
+
+	$(".video-nav-element").click(function() {
+		if ($(this).hasClass("active"))
+			return;
+
+		$(".active").removeClass("active");
+		$(this).addClass("active");
+
+		switch($(this).attr('id')) {
+			case "vid-1":
+				player.loadVideoById("QmlUKVz4nrk");
+				break;
+			case "vid-2":
+				player.loadVideoById("vPb1Xcttpp0");
+				break;
+			case "vid-3":
+				player.loadVideoById("PRi8Bamj8f8");
+				break;
+			default:
+				player.loadVideoById("EBD--b5D0ZQ");
+				break;
+		}
+	});
+
 	// Landing page switcher
 	$("#closelanding").click(function() { hide_landing(); });
 
@@ -98,3 +109,24 @@ function clean_google_sheet_json(data){
 	});
 	return formatted_json;
 }
+
+Handlebars.registerHelper("formatBodyText", function(t) {
+	t = t.trim();
+	var re = new RegExp('[\r\n]+', 'g');
+    // return (t.length>0?'<p>'+t.replace(re,'</p><p>')+'</p>':null);
+    if (t.length > 0) {
+    	var ret_val = '';
+	    for (var i = 0; i < t.length; i++) { 
+	        if (t.codePointAt(i) > 127) {
+	            ret_val += '&#' + t.codePointAt(i) + ';';
+	        } else {
+	            ret_val += t.charAt(i);
+	        }
+	    }
+    	return ('<p>'+ret_val.replace(re,'</p><p>')+'</p>');
+
+    }
+    else {
+    	return (null);
+    }
+});
